b504dcb5cd670744d6561d73a7af547b9d721643
Filipan <filipandani@gamil.com>
2020-11-08 12:45:36 +0200

    Laborator5

---
+++ Filipan_Dani_lab5/Models/ExpenseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filipan_Dani_lab5.Models
+{
+    public class ExpenseContext : DbContext
+
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
+        {}
+        public DbSet<Expense> ExpenseDTO { get; set; }
+    }
+}
